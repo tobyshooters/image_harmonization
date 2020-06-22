34399cfdf370de3891a8a3c2fda2c01d9e81edbf
Cristobal Sciutto <cristobal.sciutto@gmail.com>
2020-06-22 16:06:55 -0400

    cleanup

--- runway_model.py
+++ runway_model.py
@@ -17,6 +17,7 @@ import torch
 import cv2
 import runway
 import numpy as np
+
 from iharm.inference.predictor import Predictor
 from iharm.mconfigs import ALL_MCONFIGS
 
@@ -25,18 +26,19 @@ INPUT_SIZE = 256
 
 
 def _load_model(model_type, checkpoint):
-    net = ALL_MCONFIGS[model_type]['model'](**ALL_MCONFIGS[model_type]['params'])
-    if checkpoint is None:
-        checkpoint = torch.load("checkpoints/hrnet18_idih256.pth", map_location='cpu')
-    state = net.state_dict()
+    if type(checkpoint) is str:
+        checkpoint = torch.load(checkpoint, map_location='cpu')
+
+    model = ALL_MCONFIGS[model_type]['model'](**ALL_MCONFIGS[model_type]['params'])
+    state = model.state_dict()
     state.update(checkpoint)
-    net.load_state_dict(state)
-    return net
+    model.load_state_dict(state)
+    return model
 
 @runway.setup(options={'checkpoint': runway.file(extension='.pth')})
 def setup(opts):
-    net = _load_model(MODEL, opts['checkpoint'])
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = _load_model(MODEL, opts['checkpoint'])
     return Predictor(net, device)
 
 
@@ -52,19 +54,19 @@ outputs = {
 @runway.command('harmonize', inputs=inputs, outputs=outputs)
 def harmonize(model, inputs):
     image = np.array(inputs["composite_image"])
-    og_size = image.shape[:2]
+    image_size = image.shape[:2]
     image = cv2.resize(image, (INPUT_SIZE, INPUT_SIZE), cv2.INTER_LINEAR)
 
     mask = np.array(inputs["foreground_mask"])
     mask = cv2.resize(mask, (INPUT_SIZE, INPUT_SIZE), cv2.INTER_LINEAR)
-    mask =  mask[:, :, 0]
+    mask = mask[:, :, 0]
     mask[mask <= 100] = 0
     mask[mask > 100] = 1
     mask = mask.astype(np.float32)
 
     output = model.predict(image, mask)
-    return cv2.resize(output, og_size[::-1], cv2.INTER_LINEAR)
+    return cv2.resize(output, image_size[::-1], cv2.INTER_LINEAR)
 
 
 if __name__ == '__main__':
-    runway.run()
+    runway.run(model_options={'checkpoint': "checkpoints/hrnet18_idih256.pth"})
