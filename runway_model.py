52e33020f41eff329cbe0a86bb363721edab81b3
Cristobal Sciutto <cristobal.sciutto@gmail.com>
2020-06-22 14:28:27 -0400

    runway port

---
+++ runway_model.py
@@ -0,0 +1,57 @@
+import os
+import torch
+import cv2
+import runway
+
+from iharm.inference.predictor import Predictor
+
+# python3 scripts/predict_for_dir.py   \
+#     hrnet18_idih256                  \
+#     checkpoints/hrnet18_idih256.pth  \
+#     --images test_data/images/       \
+#     --masks test_data/masks/         \
+#     --results-path test_data/results \
+#     --gpu -1                         \
+#     --resize 256                     \
+#     --original-size                  \
+
+def _load_model(model_type):
+    net = ALL_MCONFIGS[model_type]['model'](**ALL_MCONFIGS[model_type]['params'])
+    state = net.state_dict()
+    state.update(checkpoint)
+    net.load_state_dict(state)
+    return net
+
+@runway.setup(options={'checkpoint': runway.file(extension='.pth')})
+def setup(opts):
+    net = _load_model("hrnet18_idih256", opts['checkpoint'], verbose=True)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    return Predictor(net, device)
+
+
+
+# TODO: 256 should be parameter
+# Thresholding can be cleaned up
+# Extract input fmting
+
+inputs = { 'composite_image': runway.image,
+           'foreground_mask': runway.image  }
+outputs = { 'harmonized_image': runway.image }
+
+@runway.command('harmonize', inputs=inputs, outputs=outputs)
+def harmonize(model, inputs):
+    img = np.array(inputs["composite_image"])
+    img = cv2.resize(img, (256, 256), cv2.INTER_LINEAR)
+
+    mask = np.array(inputs["foreground_mask"])
+    mask = cv2.resize(mask, (256, 256), cv2.INTER_LINEAR)
+    mask = cv2.threshold(mask[:, :, 0], 127, 255)
+    mask = mask_image[:, :, 0]
+    mask[mask <= 100] = 0
+    mask[mask > 100] = 1
+
+    return model.predict(image, mask)
+
+
+if __name__ == '__main__':
+    runway.run(port=4231)
