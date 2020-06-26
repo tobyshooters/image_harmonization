e73a538a4d2d9439e233dade36d5e313979b2e37
Ubuntu <ubuntu@p40-research-host.xrnwcpnan45e5mhwq32zv43yib.bx.internal.cloudapp.net>
2020-06-26 21:34:49 +0000

    new resolution adaptations

--- run.sh
+++ run.sh
@@ -4,10 +4,13 @@
  python3 scripts/predict_for_dir.py          \
      hrnet18_idih256                         \
      checkpoints/hrnet18_idih256.pth         \
-     --images HAdobe5k/composite_images       \
-     --masks HAdobe5k/masks              \
-     --results-path HAdobe5k/results     \
+     --images HAdobe5k/composite_images      \
+     --masks HAdobe5k/masks                  \
+     --results-path HAdobe5k/results_$1   \
+     --test-path HAdobe5k/HAdobe5k_test.txt  \
      --gpu -1                                \
-     --resize 256                            \
+     --resize $1                           \
      --original-size                         \
+     --color-transfer                        \
+     --num-inputs 10                         \
 # --------------------------------------
--- scripts/predict_for_dir.py
+++ scripts/predict_for_dir.py
@@ -16,7 +16,7 @@ from iharm.mconfigs import ALL_MCONFIGS
 from iharm.utils.log import logger
 from iharm.utils.exp import load_config_file
 
-from color_transfer import transfer_color_histogram, transfer_Lab_statistics
+from color_transfer import transfer_Lab_statistics
 
 
 def main():
@@ -44,9 +44,12 @@ def main():
     with open(args.test_path, "r") as f:
         test_files = [l.strip() for l in f.readlines()]
 
-    resize_shape = (args.resize, ) * 2
+    num_processed = 0
 
     for image_name in tqdm(image_names):
+
+        if args.num_inputs > 0 and num_processed == args.num_inputs:
+            break
 
         if image_name.split(".")[-1] != "jpg":
             continue
@@ -54,7 +57,8 @@ def main():
         if image_name not in test_files:
             continue
 
-        if image_name in already_processed:
+        file_name = image_name.split(".")[0] + "_model_output.jpg"
+        if file_name in already_processed:
             print(image_name + " already done")
             continue
 
@@ -62,12 +66,30 @@ def main():
 
         image_path = osp.join(args.images, image_name)
         image = cv2.imread(image_path)
-        og_image = image.copy()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        og_image = image.copy()
         image_size = image.shape
 
         divisor = 128
-        if resize_shape[0] > 0:
+        if args.resize > 0:
+
+            def f(h, w, r, d=128):
+                if h > w:
+                    f = round(h / r)
+                    new_h = d * (r // d)
+                    new_w = d * ((w // f) // d)
+                else:
+                    f = round(w / r)
+                    new_h = d * ((h // f) // d)
+                    new_w = d * (r // d)
+                return max(new_h, 256), max(new_w, 256)
+
+            h, w = f(image_size[0], image_size[1], args.resize)
+            print("Image shape:", image_size)
+            print("New shape:", (h, w))
+            resize_shape = (w, h)
+
+        if args.resize > 0:
             image = cv2.resize(image, resize_shape, cv2.INTER_LINEAR)
         else:
             h, w, _ = image.shape
@@ -78,7 +100,7 @@ def main():
         mask_image = cv2.imread(mask_path)
         og_mask = mask_image.copy()
 
-        if resize_shape[0] > 0:
+        if args.resize > 0:
             mask_image = cv2.resize(mask_image, resize_shape, cv2.INTER_LINEAR)
         else:
             h, w, _ = mask_image.shape
@@ -90,6 +112,7 @@ def main():
         mask[mask > 100] = 1
         mask = mask.astype(np.float32)
 
+        # Expects RGB!
         pred = predictor.predict(image, mask)
 
         if args.original_size:
@@ -98,12 +121,14 @@ def main():
         bgr_pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR).astype(np.uint8)
         _save_image(image_name, bgr_pred, flag="_model_output")
 
-        # Do color transfer
-        # output = transfer_Lab_statistics(og_image, bgr_pred, og_mask)
-        # _save_image(image_name, output, flag="_transfered_Lab")
+        if args.color_transfer:
+            assert(args.original_size)
+            # Expects RGB!
+            output = transfer_Lab_statistics(og_image, pred, og_mask)
+            output = cv2.cvtColor(output.astype(np.uint8), cv2.COLOR_RGB2BGR)
+            _save_image(image_name, output, flag="_transfered_Lab")
 
-        # output = transfer_Lab_statistics(og_image, bgr_pred, og_mask, soften=True)
-        # _save_image(image_name, output, flag="_transfered_Lab_softened")
+        num_processed += 1
 
 
 def parse_args():
@@ -139,6 +164,16 @@ def parse_args():
     parser.add_argument(
         '--test-path', type=str,
         help='Path to text file identifying tests '
+    )
+
+    parser.add_argument(
+        '--color-transfer', action='store_true', default=False,
+        help='Use color transfer to get a full-resolution image'
+    )
+
+    parser.add_argument(
+        '--num-inputs', type=int, default=-1,
+        help='Number of inputs to run'
     )
 
     args = parser.parse_args()
